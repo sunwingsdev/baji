7b8f3b1b3b0c37f6ff4cea61659c76f7d807c184
vismodeb <vismodeb2000@gmail.com>
2024-12-27 01:19:28 +0600

    Exchange menu file add

--- baji-live/src/components/exchange-page/ExchangeMiddleContent.jsx
+++ baji-live/src/components/exchange-page/ExchangeMiddleContent.jsx
@@ -1,38 +1,14 @@
-import Marquee from "react-fast-marquee";
-import { AiOutlineAudio } from "react-icons/ai";
-import { FaChrome } from "react-icons/fa";
-import { IoLogoFirefox } from "react-icons/io5";
-import kvImg from "../../assets/KV.jpg";
+import ExchangeMarquee from "./ExchangeMarquee";
+import ExchangeBanner from "./ExchangeBanner";
+import KvImage from "../../assets/KV.jpg";
+import ExchangeFooter from "./ExchangeFooter";
 
 const ExchangeMiddleContent = () => {
   return (
     <>
-      <div className="bg-[#2a3a43] opacity-90 text-white w-full py-0.5 border-b border-white">
-        <div className="flex items-center gap-4">
-          <div className="flex items-center gap-1 pl-1">
-            <AiOutlineAudio className="text-xl" />
-            <p>News</p>
-          </div>
-          <Marquee className="text-xs">
-            (Date :- 26/12/2024) .. Event : South Africa v Pakistan : Market: 50
-            Over PAK Adv {"'"}whole market voided because wrong Market offered
-            by mistake Sorry for the Inconvenience Caused
-          </Marquee>
-        </div>
-      </div>
-      <div className="">
-        <img className="w-full h-72" src={kvImg} alt="" />
-      </div>
-      <div className="mt-8">
-        <div className="flex gap-1 w-12 m-auto text-gray-400">
-          <FaChrome size={20} />
-          <IoLogoFirefox size={20} />
-        </div>
-        <p className="text-center w-[600px] m-auto">
-          Our website works best in the newest and last prior version of these
-          browsers: Google Chrome. Firefox
-        </p>
-      </div>
+      <ExchangeMarquee />
+      <ExchangeBanner img={KvImage} />
+      <ExchangeFooter />
     </>
   );
 };
