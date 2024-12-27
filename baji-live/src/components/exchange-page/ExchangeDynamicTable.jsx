ddc8681c4b2f65ee4a5e03690d08d0b78d5af1c4
vismodeb <vismodeb2000@gmail.com>
2024-12-28 01:46:32 +0600

    Exchange some problem solv and some code add

--- baji-live/src/components/exchange-page/ExchangeDynamicTable.jsx
+++ baji-live/src/components/exchange-page/ExchangeDynamicTable.jsx
@@ -30,14 +30,14 @@ const ExchangeDynamicTable = ({ tableData }) => {
                   <div className="flex flex-col items-start gap-2">
                     <Link
                       to={row?.link}
-                      className="text-blue-600 text-lg font-semibold hover:underline leading-3 whitespace-nowrap"
+                      className="text-blue-600 text-sm font-bold hover:underline leading-3 whitespace-nowrap"
                     >
                       {row?.teams}
                     </Link>
 
                     <div className="flex flex-row items-center gap-3">
                       {row?.status === "in-play" && (
-                        <p className="text-green-500 text-lg font-bold">
+                        <p className="text-green-500 text-sm font-bold">
                           In-Play
                         </p>
                       )}
@@ -50,42 +50,42 @@ const ExchangeDynamicTable = ({ tableData }) => {
                         <p className="text-slate-400 text-sm">Tomorrow 00:00</p>
                       )}
                       {row?.category === "e-cricket" && (
-                        <div className="border border-blue-700 flex flex-row items-center gap-2 w-fit pe-1 rounded-lg">
-                          <span className="bg-blue-700 text-white font-bold text-lg ps-2 pe-3 rounded-s-lg ">
+                        <div className="border border-blue-700 flex flex-row items-center gap-2 w-fit pe-2 rounded-md">
+                          <span className="bg-blue-700 text-white font-bold text-xs ps-2 pe-2 rounded-s-md ">
                             E
                           </span>
-                          <span className="text-blue-700">Cricket</span>
+                          <span className="text-blue-700 text-xs">Cricket</span>
                         </div>
                       )}
                       {row?.action === "live" && (
-                        <div className="bg-[#1a77b3] p-1.5 rounded-lg">
-                          <FaRegCirclePlay className="text-white" />
+                        <div className="bg-[#1a77b3] p-1 rounded-md">
+                          <FaRegCirclePlay className="text-white" size={10} />
                         </div>
                       )}
                       {(Array.isArray(row?.tag) ? row?.tag : [row?.tag])?.map(
                         (t, j) => (
                           <div key={j}>
                             {t === "P" && (
-                              <p className="bg-orange-500 py-0 px-2 text-white italic font-bold text-lg w-fit rounded-lg">
+                              <p className="bg-orange-500 py-0 px-1.5 text-white italic font-bold text-sm w-fit rounded-md">
                                 P
                               </p>
                             )}
                             {t === "F" && (
                               <div className="flex flex-row items-center">
-                                <div className="bg-green-500 p-1.5 rounded-s-lg">
-                                  <IoMdAlarm className="text-white" />
+                                <div className="bg-green-500 p-1 rounded-s-md">
+                                  <IoMdAlarm className="text-white" size={12} />
                                 </div>
-                                <div className="bg-[#1a77b3] py-0 px-2 text-white italic font-bold text-lg w-fit rounded-e-lg">
+                                <div className="bg-[#1a77b3] py-0 px-2 text-white italic font-bold text-sm w-fit rounded-e-md">
                                   F
                                 </div>
                               </div>
                             )}
                             {t === "B" && (
                               <div className="flex flex-row items-center">
-                                <div className="bg-green-500 p-1.5 rounded-s-lg">
-                                  <IoMdAlarm className="text-white" />
+                                <div className="bg-green-500 p-1 rounded-s-md">
+                                  <IoMdAlarm className="text-white" size={12} />
                                 </div>
-                                <div className="bg-[#1a77b3] py-0 px-2 text-white italic font-bold text-lg w-fit rounded-e-lg">
+                                <div className="bg-[#1a77b3] py-0 px-2 text-white italic font-bold text-sm w-fit rounded-e-md">
                                   B
                                 </div>
                               </div>
@@ -97,22 +97,22 @@ const ExchangeDynamicTable = ({ tableData }) => {
                   </div>
                 </div>
                 <div>
-                  <p className="text-slate-500">{row?.amount}</p>
+                  <p className="text-slate-500 text-xs">{row?.amount}</p>
                 </div>
               </div>
             </td>
             {row?.values?.map((value, j) => (
               <td
                 key={j}
-                className={`${
+                className={`text-xs ${
                   j % 2 === 0 ? "bg-blue-400" : "bg-red-300"
-                } w-20 text-center border border-white`}
+                } w-16 text-center border border-white`}
               >
                 {value}
               </td>
             ))}
-            <td className="w-12 h-14 flex items-center justify-center">
-              <MdPushPin className="border border-slate-400 rounded-full text-lg cursor-pointer hover:text-blue-500" />
+            <td className="w-12 h-12 flex items-center justify-center">
+              <MdPushPin className="text-slate-400 border border-slate-400 rounded-full text-sm cursor-pointer hover:text-blue-500" />
             </td>
           </tr>
         ))}
--- baji-live/src/pages/home/ExchangePage/Tennis.jsx
+++ baji-live/src/pages/home/ExchangePage/Tennis.jsx
@@ -1,5 +1,14 @@
+import ExchangeBanner from "@/components/exchange-page/ExchangeBanner";
+import ExchangeFooter from "@/components/exchange-page/ExchangeFooter";
+import kv03 from "../../../assets/exchange/KV03.jpg";
+
 const Tennis = () => {
-  return <div>Tennis</div>;
+  return (
+    <div>
+      <ExchangeBanner img={kv03} />
+      <ExchangeFooter />
+    </div>
+  );
 };
 
 export default Tennis;
